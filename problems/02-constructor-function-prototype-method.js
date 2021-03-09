e63d486ea2475d90a23c35a6364b3e11f45221c9
Alex Betita <alexbheb25@gmail.com>
2021-03-09 14:26:29 -0800

    finished problems 2

--- problems/02-constructor-function-prototype-method.js
+++ problems/02-constructor-function-prototype-method.js
@@ -22,9 +22,22 @@ Node.js with the examples below. Use the command:
 
 `node problems/02-constructor-function-prototype-method.js`
 
-Examples:
+***********************************************************************/
 
-const message1 = new Email('sally@smith.com', 'john@smith.com', 
+function Email(recipient, sender, subject, text){
+  this.recipient = recipient;
+  this.sender = sender;
+  this.subject = subject;
+  this.text = text;
+}
+
+Email.prototype.getSubjectAndText = function(){
+  return `${this.subject}: ${this.text}`
+}
+
+//Examples:
+
+const message1 = new Email('sally@smith.com', 'john@smith.com',
   'Test Message One', 'This is a test message.');
 console.log(message1);
 console.log(message1.getSubjectAndText());
@@ -39,7 +52,7 @@ console.log(message1.getSubjectAndText());
 // }
 // Test Message One: This is a test message.
 
-const message2 = new Email('sally@smith.com', 'john@smith.com', 
+const message2 = new Email('sally@smith.com', 'john@smith.com',
   'Test Message Two', 'This is a test message.');
 console.log(message2);
 console.log(message2.getSubjectAndText());
@@ -53,10 +66,6 @@ console.log(message2.getSubjectAndText());
 //   text: 'This is a test message.'
 // }
 // Test Message Two: This is a test message.
-
-***********************************************************************/
-
-
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
